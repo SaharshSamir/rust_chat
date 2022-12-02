de3c1a6d4a07f8add1d4138405afd4e14a05b620
Saharsh Samir <saharshsamir10@gmail.com>
2022-12-02 19:58:23 +0530

    progress

--- chat-client/src/main.rs
+++ chat-client/src/main.rs
@@ -1,55 +1,102 @@
-use std::env;
+use futures::channel::mpsc::{unbounded, UnboundedSender};
+use futures::{SinkExt, TryFutureExt};
 use futures_util::{future, pin_mut, StreamExt};
+use std::env;
+use tokio::io::{stdin, stdout};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
-use futures::channel::mpsc::{unbounded, UnboundedSender};
-use tokio::io::{stdin, stdout};
 
 #[tokio::main]
 async fn main() {
-
     //let _server_url  = env::args().nth(1).unwrap_or_else(|| panic!("pass in an argument"));
 
     let url = url::Url::parse("ws://127.0.0.1:6969").unwrap();
 
     let (stdin_tx, stdin_rx) = unbounded::<Message>();
+    let tx = stdin_tx.clone();
     //spawn a thread to take in stdin and transmit it through the unbounded channel
     tokio::spawn(handle_stdin(stdin_tx));
 
     let (ws_stream, _) = connect_async(url).await.expect("Cannot handshake");
 
-    let (ws_write, ws_read) = ws_stream.split();
-    
-    //forward stdin to the ws sink
+    let (ws_write, mut ws_read) = ws_stream.split();
+
+    // forward stdin to the ws sink
     let stdin_to_ws = stdin_rx.map(Ok).forward(ws_write);
 
-    //put what we get through the ws, to stdout
-    let ws_to_stdout = ws_read.for_each(|message| {
-        async {
-            let data = message.unwrap().into_data();
-            stdout().write_all(&data).await.unwrap();
+    // put what we get through the ws, to stdout
+    let ws_to_stdout = ws_read.for_each(|message| async {
+        match message {
+            Ok(m) => {
+                println!("---------Debug--------> {}", m);
+                match m {
+                    Message::Text(msg) => {
+                        // let data = msg;
+                        // stdin_tx.unbounded_send(Message::Ping(vec![]));
+                        let mut payload_iter = msg.splitn(2, " ");
+                        let usr_addr = payload_iter.next();
+                        let actual_message = payload_iter.next();
+                        // let (tx, rx) = unbounded::<Message>();
+                        match usr_addr {
+                            Some(usr_address) => tx
+                                .unbounded_send(Message::Ping(
+                                    String::from(usr_address).into_bytes(),
+                                ))
+                                .unwrap(),
+                            None => (),
+                        }
+
+                        match actual_message {
+                            Some(m) => println!("{}", m),
+                            None => (),
+                        }
+                        // tx.unbounded_send(Message::Ping(usr_addr.));
+                        // rx.map(Ok).forward(ws_write);
+                        // tokio::spawn(future);
+                        // stdout().write_all(&data).await.unwrap();
+                    }
+                    //recv ack
+                    Message::Pong(_) => {
+                        println!("✅");
+                    }
+                    _ => {
+                        print!("");
+                    }
+                }
+            }
+            Err(e) => println!("{:?}", e.to_string()),
         }
     });
+
+    // loop {
+    //     tokio::select! {
+    //         msg = ws_read.recv().next() {
+
+    //         }
+    //     }
+    // }
 
     pin_mut!(stdin_to_ws, ws_to_stdout);
     future::select(stdin_to_ws, ws_to_stdout).await;
 }
 
-//read data from the stdin and send it along the UnboundedSender tx 
+// async fn handle_ack(tx: UnboundedSender<Message>) {
+//     tx.unbound
+// }
+
+//read data from the stdin and send it along the UnboundedSender tx
 async fn handle_stdin(tx: UnboundedSender<Message>) {
     let mut stdin = stdin();
     loop {
         let mut buf = vec![0; 1024];
         let n = match stdin.read(&mut buf).await {
             Err(_) | Ok(0) => break,
-            Ok(n) => n
+            Ok(n) => n,
         };
         buf.truncate(n);
 
-        tx.unbounded_send(Message::binary(buf)).unwrap();
+        tx.unbounded_send(Message::Text(String::from_utf8(buf).unwrap()))
+            .unwrap();
     }
-
 }
-
-
-
+// ✅
--- chat-server/src/main.rs
+++ chat-server/src/main.rs
@@ -1,32 +1,43 @@
-#[allow(dead_code, unused_imports, unused_variables, )]
-
-use tokio_tungstenite::accept_async;
+use futures::{
+    channel::mpsc::{unbounded, UnboundedSender},
+    future, pin_mut,
+    stream::TryStreamExt,
+    SinkExt, Stream, StreamExt,
+};
+use std::{
+    collections::HashMap,
+    collections::HashSet,
+    io::Error as IoError,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+};
 use tokio::net::{TcpListener, TcpStream};
-use tokio_tungstenite::tungstenite::Message;
-use std::{collections::HashSet, collections::HashMap, sync::{Arc, Mutex}, net::SocketAddr, io::Error as IoError};
 use tokio::sync::mpsc;
-use futures::{Stream, stream::TryStreamExt, StreamExt, channel::mpsc::{unbounded, UnboundedSender}, future, SinkExt, pin_mut, };
+#[allow(dead_code, unused_imports, unused_variables)]
+use tokio_tungstenite::accept_async;
+use tokio_tungstenite::tungstenite::Message;
 
 mod client;
 
 #[derive(Eq, Hash, PartialEq, Clone)]
 struct User {
     name: String,
-    addr: SocketAddr 
+    addr: SocketAddr,
 }
 
 type Tx = UnboundedSender<Message>;
 type Degens = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
 
 #[tokio::main]
-async fn main() -> Result<(), IoError>{
-    
+async fn main() -> Result<(), IoError> {
     let peer_map = Degens::new(Mutex::new(HashMap::new()));
 
-    let mut server: TcpListener = TcpListener::bind("127.0.0.1:6969").await.expect("unable to listen");
+    let mut server: TcpListener = TcpListener::bind("127.0.0.1:6969")
+        .await
+        .expect("unable to listen");
 
     while let Ok((stream, addr)) = server.accept().await {
-        tokio::spawn(handle_me_daddy(peer_map.clone(), stream, addr));   
+        tokio::spawn(handle_me_daddy(peer_map.clone(), stream, addr));
     }
     Ok(())
 }
@@ -34,32 +45,94 @@ async fn main() -> Result<(), IoError>{
 async fn handle_me_daddy(degens: Degens, raw_stream: TcpStream, addr: SocketAddr) {
     let ws_stream = accept_async(raw_stream).await.expect("Failed ws handshake");
     println!("new degen connected, {}", addr);
-    let (bahar, andar) = ws_stream.split();  
+    let (bahar, andar) = ws_stream.split();
     let (tx, rx) = unbounded::<Message>();
 
     degens.lock().unwrap().insert(addr, tx);
 
+    //recv msg from the THE client
     let sending_to_others = andar.try_for_each(|msg| {
-       println!("message received: {}", msg.to_text().unwrap()); 
+        println!("Message Received: {}", msg.to_text().unwrap());
 
-       let peers = degens.lock().unwrap();
+        //check if ping here also
+        //if ping, send ping to the user
+        //if text, send text with addr
+        let peers = degens.lock().unwrap();
 
-       let others_but_me = peers.iter().filter(|(others_addr, _)| others_addr != &&addr).map(|(_, ws_sink)| ws_sink);
+        match msg {
+            Message::Text(s) => {
+                let others_but_me = peers
+                    .iter()
+                    .filter(|(others_addr, _)| others_addr != &&addr)
+                    .map(|(_, ws_sink)| ws_sink);
 
-       for chad in others_but_me {
-           chad.unbounded_send(msg.clone()).unwrap();
-       }
+                let mut usr_addr_str = addr.clone().to_string();
+                usr_addr_str = String::from(format!("{} ", usr_addr_str));
+                usr_addr_str.push_str(&s.to_string());
+                //send msg + address of who sent it to everyone
+                for chad in others_but_me {
+                    chad.unbounded_send(Message::Text(usr_addr_str.clone()))
+                        .unwrap();
+                }
+            }
+            Message::Ping(p) => {
+                //send to ping to who it's meant to ping
+                let urs_addr: SocketAddr = String::from_utf8(p).unwrap().parse().unwrap();
 
-       future::ok(())
+                let usr_to_send_ack = peers
+                    .iter()
+                    .filter(|(ad, _)| ad == &&urs_addr)
+                    .map(|(_, ws_sink)| ws_sink);
+
+                for ping_user in usr_to_send_ack {
+                    ping_user
+                        // .unbounded_send(Message::Ping(String::from().into_bytes()))
+                        .unbounded_send(Message::Ping(urs_addr.to_string().into_bytes()))
+                        .unwrap();
+                }
+            }
+            _ => (),
+        }
+
+        future::ok(())
     });
 
-    let recv_from_others = rx.map(Ok).forward(bahar);
+    let recv_from_others = rx
+        .map(|msg| {
+            let msg = match msg {
+                Message::Ping(ack) => {
+                    let ack_print = ack.clone();
+                    println!("at recv from others{:?}", ack_print);
+                    println!("{}", String::from_utf8(ack_print).unwrap());
+                    let usr_addr: SocketAddr = String::from_utf8(ack).unwrap().parse().unwrap();
+                    let peers = degens.lock().unwrap();
+
+                    let usr_to_send_ack = peers
+                        .iter()
+                        .filter(|(others_addr, _)| others_addr == &&usr_addr)
+                        .map(|(_, ws_sink)| ws_sink);
+
+                    for usr in usr_to_send_ack {
+                        usr.unbounded_send(Message::Pong(usr_addr.to_string().into_bytes()))
+                            .unwrap();
+                    }
+                    // Message::Text(String::from(""))
+                    Message::Ping(String::from("").into_bytes())
+                }
+                Message::Text(t) => Message::Text(t),
+                Message::Binary(_) => todo!(),
+                Message::Pong(p) => Message::Pong(p),
+                Message::Close(_) => todo!(),
+                Message::Frame(_) => todo!(),
+            };
+            return Ok(msg);
+        })
+        .forward(bahar);
+    // let recv_from_others = rx.map(|what| what).forward(bahar);
     pin_mut!(sending_to_others, recv_from_others);
     future::select(sending_to_others, recv_from_others).await;
-    
+
     println!("{} disconnected", &addr);
 
     degens.lock().unwrap().remove(&addr);
 }
-
-
